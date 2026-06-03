cd536809410a4479817da28850651d27b1759335
James C Tanner <whereischason@protonmail.com>
2026-06-03 18:47:17 -0400

    Include JetBrains Mono in font packages
    Add JetBrains Mono font to the font packages.

--- configuration.nix
+++ configuration.nix
@@ -50,7 +50,10 @@
         waybar
 		neovim
     ];  
-
+ 	fonts.packages = with pkgs; [
+    	nerd-fonts.jetbrains-mono
+ 	 ];
+	
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
     system.stateVersion = "26.05"; 
